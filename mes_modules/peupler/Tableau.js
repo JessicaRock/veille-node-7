e8109c6fccb51e1ffc759667919e05d22affbcf1
Jessica Rock <e1530023@cmaisonneuve.qc.ca>
2018-02-20 16:06:21 -0500

    Ajout de la fonction peupler et test de la fonction - Réussi

---
+++ mes_modules/peupler/Tableau.js
@@ -0,0 +1,57 @@
+"use strict";
+let tableau = [
+	"Mercier",
+	"Châteauguay",
+	"Léry",
+	"Beauharnois",
+	"Valleyfield",
+	"Ormstown",
+	"Howick",
+	"Sainte-Catherine",
+	"Brossard"
+];
+
+let courriel = [
+	"gmail.com",
+	"hotmail.com",
+	"videotron.ca",
+	"sympatico.ca",
+	"yahoo.ca"
+];
+
+let prenom = [
+	"Amélie",
+	"Olivier",
+	"Nathan",
+	"Jessica",
+	"Simon",
+	"Luc",
+	"Carole",
+	"Yves",
+	"Mélanie",
+	"Caroline",
+	"Chloé",
+	"Gisèle",
+	"Lorraine",
+	"Guy"
+];
+
+let nom = [
+	"Boyer",
+	"Doyle",
+	"Sioui",
+	"Rock",
+	"Tremblay",
+	"Turcotte",
+	"Gélinas",
+	"Roch",
+	"L'Écuyer",
+	"Tremblay",
+	"Chartier",
+	"Huot"
+];
+
+//module.exports = tableau;
+module.exports.courriel = courriel;
+module.exports.prenom = prenom;
+module.exports.nom = nom;
---
+++ mes_modules/peupler/index.js
@@ -0,0 +1,44 @@
+"use strict";
+const nbEnreg = 10;
+const tableaux = require('./Tableau.js');
+//const max = tableaux.prenom;
+//console.log('max = ' + max);
+
+const peupler_json = () => {
+	console.log('ok');
+
+	let aoNouvPers = [];
+	let position, chiffre;
+	for(let i = 0; i < nbEnreg; i++) {
+		aoNouvPers[i] = {};
+
+		position = Math.floor(Math.random()*tableaux.prenom.length);
+		aoNouvPers[i]['prenom'] = tableaux.prenom[position];
+
+
+		position = Math.floor(Math.random()*tableaux.nom.length);
+		aoNouvPers[i]['nom'] = tableaux.nom[position];
+
+		position = Math.floor(Math.random()*tableaux.courriel.length);
+		aoNouvPers[i]['courriel'] = aoNouvPers[i]['prenom'] + '.' + aoNouvPers[i]['nom'] + '@' + tableaux.courriel[position];
+
+
+		let tel = "";
+		for(let iCompteur = 0; iCompteur<10; iCompteur++) {
+
+			chiffre = Math.floor(Math.random()*10);
+			tel += chiffre;
+		}
+
+		aoNouvPers[i]['telephone'] = tel;
+
+
+
+		//tabVille.push(tableau[position]);
+
+	}
+
+	return aoNouvPers;
+}
+
+module.exports = peupler_json;
